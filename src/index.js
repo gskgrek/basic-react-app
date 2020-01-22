09f9fd626d09abb1ad7ee7fa033d5a23eb119895
gskgrek <biuro@greku.pl>
2020-01-22 22:14:03 +0100

    creat react app basic setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+
+import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
